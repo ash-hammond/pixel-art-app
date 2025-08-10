c02d74f449a12f3c9c63809ef2dd69664f8b909c
Ash Hammond <ash_hammond@fastmail.com>
2025-08-10 14:46:16 -0500

    canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from "next/image";
 import {Property} from "csstype";
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function ColorButton({setColor, color}: {setColor: (color: Property.BackgroundColor) => void, color: Property.BackgroundColor}) {
     return <button onClick={() => setColor(color)} style={{backgroundColor: color}} className="h-6 w-6 rounded-2xl cursor-pointer"></button>
@@ -9,6 +9,16 @@ function ColorButton({setColor, color}: {setColor: (color: Property.BackgroundCo
 
 function ColorBlock({color}: {color: Property.BackgroundColor}) {
     return <div style={{backgroundColor: color}} className="h-6 w-6 rounded-2xl"></div>
+}
+
+function PixelCanvas({width, height}) {
+    const ref = useRef<HTMLCanvasElement>(null);
+    useEffect(() => {
+        const ctx = ref.current!.getContext("2d")!
+        ctx.fillStyle = "red"
+        ctx.fillRect(0, 0, 100, 100)
+    })
+    return <canvas ref={ref} width={width} height={height}></canvas>
 }
 
 export default function Home() {
@@ -19,6 +29,7 @@ export default function Home() {
           <p>hi</p>
           <ColorBlock color={color}></ColorBlock>
           {pallete.map((color, i) => <ColorButton key={i} setColor={setColor} color={color}></ColorButton>)}
+          <PixelCanvas width={100} height={100}></PixelCanvas>
       </div>
   );
 }
