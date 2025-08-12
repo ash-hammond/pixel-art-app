d53a3d687410fbdf83e3f165cccc589347630459
Ash Hammond <ash_hammond@fastmail.com>
2025-08-12 08:27:18 -0500

    basic click and paint

--- app/page.tsx
+++ app/page.tsx
@@ -11,19 +11,28 @@ function ColorBlock({color}: {color: Property.BackgroundColor}) {
     return <div style={{backgroundColor: color}} className="h-6 w-6 rounded-2xl"></div>
 }
 
-function PixelCanvas({width, height, scale}) {
+function PixelCanvas({width, height, scale, picked_color}) {
     const ref = useRef<HTMLCanvasElement>(null);
     useEffect(() => {
         const canvas = ref.current!
         const ctx = canvas.getContext("2d")!
         ctx.fillStyle = "red"
+        function fillPixel(x, y, color) {
+            ctx.fillStyle = color
+            ctx.fillRect(x * scale, y * scale, scale, scale);
+        }
         ctx.fillRect(0, 0, width * scale, height * scale)
         console.log("position", canvas.getBoundingClientRect())
+        let mouse_x = 0
+        let mouse_y = 0
         canvas.addEventListener("mousemove", (event) => {
             const rect = canvas.getBoundingClientRect()
-            const x = Math.floor((event.x - rect.x) / scale)
-            const y = Math.floor((event.y - rect.y) / scale)
-            console.log(x, y)
+            mouse_x = Math.floor((event.x - rect.x) / scale)
+            mouse_y = Math.floor((event.y - rect.y) / scale)
+        })
+        canvas.addEventListener("click", (event) => {
+            console.log("clicked", mouse_x, mouse_y)
+            fillPixel(mouse_x, mouse_y, picked_color);
         })
     })
     return <canvas ref={ref} width={width * scale} height={height * scale}></canvas>
@@ -34,10 +43,9 @@ export default function Home() {
     const [color, setColor] = useState<Property.BackgroundColor>("red");
     return (
       <div>
-          <p>hi</p>
           <ColorBlock color={color}></ColorBlock>
           {pallete.map((color, i) => <ColorButton key={i} setColor={setColor} color={color}></ColorButton>)}
-          <PixelCanvas width={64} height={64} scale={10}></PixelCanvas>
+          <PixelCanvas width={64} picked_color={color} height={64} scale={10}></PixelCanvas>
       </div>
   );
 }
